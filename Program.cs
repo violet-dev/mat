b388b1278e50a86a1eab7b7e6232b32651535c55
violet-dev <violet.dev.master@gmail.com>
2021-08-24 02:29:14 +0000

    init: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace matcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
